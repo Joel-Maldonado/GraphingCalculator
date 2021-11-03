bf3b1ec848db243eaca4c2a2ed2568509c5d2c10
Joel <jmaldonadoruiz0@gmail.com>
2021-11-02 19:37:05 -0700

    Added support for fraction y values

--- GraphingCalculator.java
+++ GraphingCalculator.java
@@ -1,37 +1,28 @@
 import javax.swing.*;
 import java.awt.*;
-import java.util.Scanner;
 
 public class GraphingCalculator extends JComponent {
-    static int units = 25;
-    static int plotPointSize = 5;
-    static int min;
-    static int max;
+    static int units = 30;
+    static int plotPointSize = units / 5;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Graphing Calulator");
         GraphingCalculator shapes = new GraphingCalculator();
-        Scanner kb = new Scanner(System.in);
-
-        System.out.print("Min Range (X): ");
-        min = Integer.parseInt(kb.nextLine());
-
-        System.out.print("Min Range (X): ");
-        max = Integer.parseInt(kb.nextLine());
 
         frame.setSize(800, 800);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.add(shapes);
         frame.setVisible(true);
-
-        kb.close();
     }
 
     public void paint(Graphics g) {
-        int xOffset = getWidth()/2;
-        int yOffset = getHeight()/2;
-
+        int min = -getWidth() / units / 2;
+        int max = getWidth() / units / 2;
+        
+        int xOffset = getWidth() / 2;
+        int yOffset = getHeight() / 2;
+        
         // Draw Grid
         g.setColor(new Color(0, 0, 0, 100));
 
@@ -65,13 +56,13 @@ public class GraphingCalculator extends JComponent {
         }
 
         // Plot Points
-        g.setColor(new Color(0, 0, 255));
+        g.setColor(Color.blue);
         for (int x = min; x <= max; x++) {
-            g.fillRect(x * units + xOffset - plotPointSize/2, -f(x) * units + yOffset - plotPointSize/2, plotPointSize, plotPointSize);
+            g.fillRect(x * units + xOffset - plotPointSize/2, (int)Math.round(-f(x) * units) + yOffset - plotPointSize/2, plotPointSize, plotPointSize);
         }
     }
 
-    public static int f(int x) {
-        return 5;
+    public static double f(double x) {
+        return x / 2;
     }
 }
